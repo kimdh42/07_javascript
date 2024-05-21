1a334c2690e5dd29f3941936e90ca7444f8ae8e5
kimdh42 <kimdh1992@gmail.com>
2024-05-21 18:20:54 +0900

    js

--- 01_core/03_operator/03_optional-chaining-operator.js
+++ 01_core/03_operator/03_optional-chaining-operator.js
@@ -2,7 +2,7 @@
 
 /* 
     ES11(ECMAScript2020)에서 도입된 연산자로
-    좌항의 피연산자가 null 또는 underined인 경우 undefined를 반환하고
+    좌항의 피연산자가 null 또는 undefined인 경우 undefined를 반환하고
     그렇지 않으면 우항의 프로퍼티 참조를 이어간다.
 */
 
